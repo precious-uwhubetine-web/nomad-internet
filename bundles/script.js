cc60ce137cc7277babc4ae8be45c617ae73a3667
Precious Uwhubetine <preciousbetine@gmail.com>
2026-01-06 22:36:39 +0100

    Add bundles home page

---
+++ bundles/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const menuButton = document.getElementById('bundles-header-content-menu-button');
+  const mobileMenu = document.getElementById('bundles-header-mobile-menu');
+
+  const closeIcon = document.getElementById('bundles-header-mobile-menu-close-icon');
+  const hamburgerIcon = document.getElementById('bundles-header-mobile-menu-hamburger-icon');
+
+  menuButton.addEventListener('click', function() {
+    if (mobileMenu.style.display === 'flex') {
+      mobileMenu.style.display = 'none';
+      hamburgerIcon.style.display = 'inline';
+      closeIcon.style.display = 'none';
+    } else {
+      mobileMenu.style.display = 'flex';
+      hamburgerIcon.style.display = 'none';
+      closeIcon.style.display = 'inline';
+    }
+  });
+});
+
+window.addEventListener("resize", function() {
+  const mobileMenu = document.getElementById('bundles-header-mobile-menu');
+  if (window.innerWidth > 768) {
+    mobileMenu.style.display = 'none';
+    const hamburgerIcon = document.getElementById('bundles-header-mobile-menu-hamburger-icon');
+    const closeIcon = document.getElementById('bundles-header-mobile-menu-close-icon');
+    hamburgerIcon.style.display = 'inline';
+    closeIcon.style.display = 'none';
+  }
+});
